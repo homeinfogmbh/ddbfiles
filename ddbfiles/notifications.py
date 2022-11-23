00b0b98a963e9adf4070dc23b1ceaca2c84f4856
Richard Neumann <r.neumann@homeinfo.de>
2022-11-23 11:13:14 +0100

    Use inotify watchdog

--- ddbfiles/notifications.py
+++ ddbfiles/notifications.py
@@ -1,19 +1,21 @@
 """Notifications about file updates."""
 
+from argparse import ArgumentParser, Namespace
 from typing import Iterable
 
 from configlib import load_config
 from emaillib import EMail, Mailer
+from functools import partial
 from his import Account, stakeholders
 
 
 __all__ = ['notify']
 
 
-TEXT = '''Sehr geehrte Empfänger:innen,
+TEXT_TEMPLATE = '''Sehr geehrte Empfänger:innen,
 
-es gibt neue DDB-Dateien zum herunter laden.
-Besuchen Sie dazu: {url}
+es gibt neue DDB-Dateien zum herunter laden:
+{filename}
 
 Mit freundlichen Grüßen
 
@@ -21,22 +23,25 @@ Ihre HOMEINFO GmbH
 '''
 
 
-def notify(accounts: Iterable[Account]) -> None:
+def notify(accounts: Iterable[Account], filename: str) -> None:
     """Notify the given accounts."""
 
     get_mailer().send(
-        map(generate_email, map(lambda account: account.email, accounts))
+        map(
+            partial(generate_email, filename=filename),
+            map(lambda account: account.email, accounts)
+        )
     )
 
 
-def generate_email(recipient: str) -> EMail:
+def generate_email(recipient: str, filename: str) -> EMail:
     """Yield emails for the respective recipients."""
 
     return EMail(
         subject='Neue DDB Dateien',
         sender='automailer@homeifno.de',
         recipient=recipient,
-        plain=TEXT
+        plain=TEXT_TEMPLATE.format(filename=filename)
     )
 
 
@@ -46,7 +51,16 @@ def get_mailer() -> Mailer:
     return Mailer.from_config(load_config('ddbfiles.conf'))
 
 
+def get_args(description: str = __doc__) -> Namespace:
+    """Return the parsed command line arguments."""
+
+    parser = ArgumentParser(description=description)
+    parser.add_argument('filename', help='name of the new file')
+    return parser.parse_args()
+
+
 def main() -> None:
     """Notify all stakeholders."""
 
-    notify(stakeholders('ddbfiles'))
+    args = get_args()
+    notify(stakeholders('ddbfiles'), args.filename)
