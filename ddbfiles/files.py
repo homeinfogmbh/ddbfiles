45fb507f281169ab34f3ef88b0a2a76bcbac1bbd
Richard Neumann <r.neumann@homeinfo.de>
2023-03-22 10:55:41 +0100

    Only notify about DDB images

--- ddbfiles/files.py
+++ ddbfiles/files.py
@@ -5,14 +5,15 @@ from pathlib import Path
 from ddbfiles.file import File
 from ddbfiles.glob import Glob
 
-__all__ = ['FILES']
+__all__ = ['DDB_ISO_REGEX', 'FILES']
 
 
 BASEDIR = Path('/usr/local/share/ddbfiles')
+DDB_ISO_REGEX = r'HIDSL-(.+)-x86_64\.iso'
 FILES = {
     'HIDSL': File(
         Glob(BASEDIR, 'HIDSL-*-x86_64.iso'),
-        r'HIDSL-(.+)-x86_64\.iso'
+        DDB_ISO_REGEX
     ),
     'HIDSL-ARM': File(
         Glob(BASEDIR, 'HIDSL-arm-raspi4-*.tar.lzop'),
--- ddbfiles/notifications.py
+++ ddbfiles/notifications.py
@@ -2,28 +2,46 @@
 
 from argparse import ArgumentParser, Namespace
 from functools import partial
+from pathlib import Path
+from re import match
 from typing import Iterable
 
 from configlib import load_config
 from emaillib import EMail, Mailer
 from his import Account, stakeholders
 
+from ddbfiles.files import DDB_ISO_REGEX
+
 __all__ = ['notify']
 
 
-DOWNLOAD_URL = 'https://sysmon2.homeinfo.de/dashboard.html '
-TEXT_TEMPLATE = f'''Sehr geehrte Empfänger:innen,
+TEXT_TEMPLATE = '''Sehr geehrte Damen und Herren, 
 
-es gibt eine neu DDB-Datei zum herunter laden:
+wir freuen uns, Ihnen mitteilen zu können, dass die neueste Version unseres DDB Images jetzt zum Download bereitsteht. 
 
-* {{filename}}
+Wir haben neue Funktionen hinzugefügt, die Ihnen mehr Flexibilität und Kontrolle bieten. Wenn Sie bereits eine vorherige Version des DDB Images verwenden, empfehlen wir Ihnen, dieses Update so bald wie möglich herunterzuladen.  
 
-Sie können die Datei unter {DOWNLOAD_URL}
-herunter laden.
+Die aktuelle Version ist: {filename}
+Zum Download loggen Sie sich bitte im Sysmon unter folgendem Link ein: und finden Sie das Image dort im Bereich Download auf der Dashboard Seite 
+https://sysmon2.homeinfo.de/dashboard.html
 
-Mit freundlichen Grüßen
+Vielen Dank für Ihr Interesse an unserem Produkt. Wir freuen uns auf Ihre Rückmeldungen und Fragen. 
 
-Ihre HOMEINFO GmbH
+Mit freundlichen Grüßen, 
+HOMEINFO 
+Digitale Informationssysteme GmbH 
+Spörckenstr. 11 
+
+30419 Hannover 
+
+Fon.: 0511 21 24 11 00 
+Web : www.homeinfo.de
+
+Amtsgericht Hannover HRB 58516 
+Ust - IDNr.:  DE 206025922 
+Geschäftsführer: 
+Marcus Berbic 
+Patrick Gunkel
 '''
 
 
@@ -42,7 +60,7 @@ def generate_email(recipient: str, filename: str) -> EMail:
     """Yield emails for the respective recipients."""
 
     return EMail(
-        subject='Neue DDB Dateien',
+        subject='Version des DDB Images',
         sender='noreply@homeinfo.de',
         recipient=recipient,
         plain=TEXT_TEMPLATE.format(filename=filename)
@@ -59,7 +77,7 @@ def get_args(description: str = __doc__) -> Namespace:
     """Return the parsed command line arguments."""
 
     parser = ArgumentParser(description=description)
-    parser.add_argument('filename', help='name of the new file')
+    parser.add_argument('filename', type=Path, help='name of the new file')
     return parser.parse_args()
 
 
@@ -67,4 +85,6 @@ def main() -> None:
     """Notify all stakeholders."""
 
     args = get_args()
-    notify(stakeholders('ddbfiles'), args.filename)
+
+    if match(DDB_ISO_REGEX, args.filename.name):
+        notify(stakeholders('ddbfiles'), args.filename)
