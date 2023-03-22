17517ce75c19fc7deb333c2ffe218828f74e8e2f
Richard Neumann <r.neumann@homeinfo.de>
2023-03-22 10:59:03 +0100

    Outsource template to file

--- ddbfiles/notifications.py
+++ ddbfiles/notifications.py
@@ -12,37 +12,18 @@ from his import Account, stakeholders
 
 from ddbfiles.files import DDB_ISO_REGEX
 
+
 __all__ = ['notify']
 
 
-TEXT_TEMPLATE = '''Sehr geehrte Damen und Herren, 
+TEMPLATE_FILE = Path('/usr/local/etc/ddbfiles.email.template')
 
-wir freuen uns, Ihnen mitteilen zu können, dass die neueste Version unseres DDB Images jetzt zum Download bereitsteht. 
 
-Wir haben neue Funktionen hinzugefügt, die Ihnen mehr Flexibilität und Kontrolle bieten. Wenn Sie bereits eine vorherige Version des DDB Images verwenden, empfehlen wir Ihnen, dieses Update so bald wie möglich herunterzuladen.  
+def get_template() -> str:
+    """Return the email body template."""
 
-Die aktuelle Version ist: {filename}
-Zum Download loggen Sie sich bitte im Sysmon unter folgendem Link ein: und finden Sie das Image dort im Bereich Download auf der Dashboard Seite 
-https://sysmon2.homeinfo.de/dashboard.html
-
-Vielen Dank für Ihr Interesse an unserem Produkt. Wir freuen uns auf Ihre Rückmeldungen und Fragen. 
-
-Mit freundlichen Grüßen, 
-HOMEINFO 
-Digitale Informationssysteme GmbH 
-Spörckenstr. 11 
-
-30419 Hannover 
-
-Fon.: 0511 21 24 11 00 
-Web : www.homeinfo.de
-
-Amtsgericht Hannover HRB 58516 
-Ust - IDNr.:  DE 206025922 
-Geschäftsführer: 
-Marcus Berbic 
-Patrick Gunkel
-'''
+    with TEMPLATE_FILE.open('r', encoding='utf-8') as file:
+        return file.read()
 
 
 def notify(accounts: Iterable[Account], filename: str) -> None:
@@ -63,7 +44,7 @@ def generate_email(recipient: str, filename: str) -> EMail:
         subject='Version des DDB Images',
         sender='noreply@homeinfo.de',
         recipient=recipient,
-        plain=TEXT_TEMPLATE.format(filename=filename)
+        plain=get_template().format(filename=filename)
     )
 
 
