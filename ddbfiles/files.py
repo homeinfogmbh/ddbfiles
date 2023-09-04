f49d7a7744dcbb2c443680fe1232d881fb49d434
Tobias Erlacher <40563361+aterriblechaos@users.noreply.github.com>
2023-09-04 12:53:05 +0200

    Update files.py
    API for DDBOS ISO in sysmon

--- ddbfiles/files.py
+++ ddbfiles/files.py
@@ -10,8 +10,10 @@ __all__ = ["DDB_ISO_REGEX", "FILES"]
 
 BASEDIR = Path("/usr/local/share/ddbfiles")
 DDB_ISO_REGEX = r"HIDSL-(.+)-x86_64\.iso"
+DDBOS_ISO_REGEX = r"DDBOS-(.+)-x86_64\.iso"
 FILES = {
     "HIDSL": File(Glob(BASEDIR, "HIDSL-*-x86_64.iso"), DDB_ISO_REGEX),
+    "DDBOS": File(Glob(BASEDIR, "DDBOS-*-x86_64.iso"), DDBOS_ISO_REGEX),
     "HIDSL-ARM": File(
         Glob(BASEDIR, "HIDSL-arm-raspi4-*.tar.lzop"),
         r"HIDSL-arm-raspi4-(.+)\.tar\.lzop",
